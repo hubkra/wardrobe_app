f5f2fecec6ce4f6977431f353b226683abd358bf
Hubert Kras <goodguy.kras@gmail.com>
2023-09-25 13:05:47 +0200

    fix the view

--- lib/widgets/profile_page.dart
+++ lib/widgets/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'dart:typed_data';
 import 'dart:convert';
 
@@ -15,13 +16,13 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  late User _user;
+  late Future<User> _userFuture;
   final UserApiService userApiService = UserApiService();
 
   @override
   void initState() {
     super.initState();
-    _fetchUser(widget.username);
+    _userFuture = _fetchUser(widget.username);
   }
 
   Uint8List decodeImageString(String imageString) {
@@ -29,119 +30,121 @@ class _ProfilePageState extends State<ProfilePage> {
     return Uint8List.fromList(decodedBytes);
   }
 
-  Future<void> _fetchUser(String username) async {
+  Future<User> _fetchUser(String username) async {
     try {
       User? user = await userApiService.getUser(username);
+      if (user != null) {
+        return user;
+      } else {
+        throw Exception('Użytkownik o podanym adresie e-mail nie istnieje');
+      }
+    } catch (e) {
+      throw Exception('Błąd podczas pobierania użytkownika: $e');
+    }
+  }
+
+  Future<void> _selectNewAvatar() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      final filePath = pickedFile.path;
+      final user = await userApiService.getUser(widget.username);
 
       if (user != null) {
-        _user = user;
+        final response =
+            await userApiService.uploadProfilePicture(user, filePath);
 
-        if (_user!.profilePicture != null) {
-          Uint8List? imageBytes = decodeImageString(_user!.profilePicture!);
+        if (response == 'Zdjęcie użytkownika zostało zaktualizowane.') {
+          _fetchUser(widget.username);
+        } else {
+          print(response);
         }
       } else {
         print('Użytkownik o podanym adresie e-mail nie istnieje');
       }
-    } catch (e) {
-      print('Błąd podczas pobierania użytkownika: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
+      body: FutureBuilder<User>(
+        future: _userFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Wystąpił błąd: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData) {
+            return Center(
+              child: Text('Użytkownik o podanym adresie e-mail nie istnieje'),
+            );
+          } else {
+            final user = snapshot.data!;
+
+            return ListView(
               children: [
-                CircleAvatar(
-                  radius: 80.0,
-                  backgroundColor: Colors.transparent,
-                  child: ClipOval(
-                    child: _user.profilePicture != null
-                        ? Image.memory(
-                            Uint8List.fromList(
-                                decodeImageString(_user.profilePicture!)),
-                            fit: BoxFit.cover,
-                            width: 160,
-                            height: 160,
-                          )
-                        : Image.asset(
-                            'assets/your_image.png',
-                            fit: BoxFit.cover,
-                            width: 160,
-                            height: 160,
-                          ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(50.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 80.0,
+                        backgroundColor: Colors.transparent,
+                        child: ClipOval(
+                          child: user.profilePicture != null
+                              ? Image.memory(
+                                  Uint8List.fromList(
+                                      decodeImageString(user.profilePicture!)),
+                                  fit: BoxFit.cover,
+                                  width: 160,
+                                  height: 160,
+                                )
+                              : Image.asset(
+                                  'assets/your_image.png',
+                                  fit: BoxFit.cover,
+                                  width: 160,
+                                  height: 160,
+                                ),
+                        ),
+                      ),
+                      const SizedBox(height: 24.0),
+                      const Text(
+                        'Change Avatar',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 24.0),
-                const Text(
-                  'Change Avatar',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
+                const SizedBox(height: 100.0),
+                ListTile(
+                  title: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _selectNewAvatar();
+                      },
+                      child: const Text('Change Avatar'),
+                    ),
                   ),
+                  tileColor: Colors.purple[100],
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
+                  onTap: () {},
                 ),
+                // Reszta widoku
               ],
-            ),
-          ),
-          const SizedBox(height: 100.0),
-          ListTile(
-            title: const Center(
-              child: Text(
-                'Change',
-                style: TextStyle(
-                  fontSize: 32.0,
-                ),
-              ),
-            ),
-            tileColor: Colors.purple[100], // Purple background color
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 8.0,
-            ), // Add margin
-            onTap: () {
-              // Implement action for Setting 1
-            },
-          ),
-          ListTile(
-            title: const Center(
-              child: Text(
-                'Setting 2',
-                style: TextStyle(
-                  fontSize: 32.0,
-                ),
-              ),
-            ),
-            tileColor: Colors.purple[100], // Purple background color
-            contentPadding: const EdgeInsets.symmetric(
-                horizontal: 16.0, vertical: 8.0), // Add margin
-            onTap: () {
-              // Implement action for Setting 2
-            },
-          ),
-          ListTile(
-            title: const Center(
-              child: Text(
-                'Setting 3',
-                style: TextStyle(
-                  fontSize: 32.0,
-                ),
-              ),
-            ),
-            tileColor: Colors.purple[100], // Purple background color
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 8.0,
-            ), // Add margin
-            onTap: () {
-              // Implement action for Setting 3
-            },
-          ),
-        ],
+            );
+          }
+        },
       ),
     );
   }
